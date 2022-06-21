3987c46c364400b4ae20053df59e8fa5de86f2ef
Thayanne França da Silva <thayanne.silva@dellead.com>
2022-06-21 10:30:08 -0300

    Adicionando dataset e carregando dados

---
+++ Segmentacao_de_Clientes/marketing.R
@@ -0,0 +1,28 @@
+# Segmentação de Clientes com Base em Análise RFM (Recência, Frequência e Valor Monetário)
+
+
+# Imports
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+library(caret)
+library(plotly)
+library(readxl)
+library(rfm)
+library(stats)
+library(factoextra)
+
+# Função para carregar os dados da planilha Excel
+carrega_dados <- function()
+{
+  setwd("~/projetos_DS_R/Segmentacao_de_Clientes")
+  sheet1 <- read_excel('online_retail_II.xlsx', sheet = 'Year 2009-2010')
+  sheet2 <- read_excel('online_retail_II.xlsx', sheet = 'Year 2010-2011')
+  dados_combinados <- rbind(sheet1, sheet2)
+  return(dados_combinados)
+}
+
+# Executa a função
+dados <- carrega_dados()
+dim(dados)
+View(dados)
