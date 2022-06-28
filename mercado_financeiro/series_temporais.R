49ee441612d0faa077c2e67b146b231ad1fa8c6d
Thayanne França da Silva <thayanne.silva@dellead.com>
2022-06-28 10:15:06 -0300

    Adicionando arquivo em R e instalando as bibliotecas necessárias

---
+++ mercado_financeiro/series_temporais.R
@@ -0,0 +1,10 @@
+setwd("~/projetos_DS_R/mercado_financeiro")
+getwd()
+
+# Instalar e carregar os pacotes
+install.packages("quantmod")
+install.packages("xts")
+install.packages("moments")
+library(quantmod)
+library(xts)
+library(moments)
