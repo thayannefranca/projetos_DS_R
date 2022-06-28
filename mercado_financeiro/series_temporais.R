241b5a4b147a822a3c4f199626e346f9a92c589d
Thayanne França da Silva <thayanne.silva@dellead.com>
2022-06-28 10:17:37 -0300

    Selecionando o periodo de tempo

--- mercado_financeiro/series_temporais.R
+++ mercado_financeiro/series_temporais.R
@@ -8,3 +8,8 @@ install.packages("moments")
 library(quantmod)
 library(xts)
 library(moments)
+
+# Seleção do período de análise
+startDate = as.Date("2018-01-21")
+endDate = as.Date("2018-06-21")
+
