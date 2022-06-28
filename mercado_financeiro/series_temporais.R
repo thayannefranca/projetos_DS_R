0a1d6a7d2c64f95cac7643e76091d6709a65bf1f
Thayanne França da Silva <thayanne.silva@dellead.com>
2022-06-28 10:20:45 -0300

    Download dos dados do período e checando o tipo de dados

--- mercado_financeiro/series_temporais.R
+++ mercado_financeiro/series_temporais.R
@@ -13,3 +13,11 @@ library(moments)
 startDate = as.Date("2018-01-21")
 endDate = as.Date("2018-06-21")
 
+# Download dos dados do período
+# Obs: O Yahoo Finance está passando por mudanças 
+# e o serviço de cotações online pode estar instável
+getSymbols("PETR4.SA", src = "yahoo", from = startDate, to = endDate, auto.assign = T)
+
+# Checando o tipo de dado retornado
+class(PETR4.SA)
+is.xts(PETR4.SA)
